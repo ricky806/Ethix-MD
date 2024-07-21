9bac0ed9ab35f199075f2a42162cd38825d02326
ricky806 <117099405+ricky806@users.noreply.github.com>
2024-07-21 08:52:07 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&KcXFqb5b",
+  SESSION_ID: process.env.SESSION_ID || "ETHIX-MD&Qt1VwgX4",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
@@ -17,7 +17,7 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "public",
+  MODE: process.env.MODE || "private",
   OWNER_NAME: process.env.OWNER_NAME || "𝛥𝐿𝛯𝛸 𝛲𝛪𝛫𝑈",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "918536881026",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
